394bad8a5e6f1e34be5106377ee33aa0e0f2c0e1
Gabriel Varadi <gabriel.varadi.hallope.lecoq@gmail.com>
2019-06-03 22:11:39 +0000

    Set up the project, update package.json

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,31 @@
 import React, {Component} from 'react';
+import ChatBar from './ChatBar.jsx'
+import NavBar from './NavBar.jsx'
+import MessageList from './MessageList.jsx'
+
+
 
 class App extends Component {
+
+  constructor(props) {
+  super(props);
+  this.state = {
+    currentUser: {name:'Bob'},
+    messages: [
+    {
+      username: 'Bob',
+      content: 'text text text text text',
+    }
+  ]
+}
+}
   render() {
     return (
-      <h1>Hello React :)</h1>
+    <div>
+      <NavBar/>
+        <MessageList/>
+      <ChatBar/>
+      </div>
     );
   }
 }
